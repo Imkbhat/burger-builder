27526d0acfc51ce83622fc6288be2bd6edaae113
Imkbhat <kbs71190@gmail.com>
2020-08-03 16:45:47 +0530

    Updates

---
+++ build/precache-manifest.a5706ecc373e48cfdf22819400701fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db6fc53883bba909d088",
+    "url": "/burger-builder/static/css/main.d158774a.chunk.css"
+  },
+  {
+    "revision": "db6fc53883bba909d088",
+    "url": "/burger-builder/static/js/main.db6fc538.chunk.js"
+  },
+  {
+    "revision": "95aae902d443e9981fb8",
+    "url": "/burger-builder/static/js/runtime~main.95aae902.js"
+  },
+  {
+    "revision": "dc799eb5c1ec6c433e5c",
+    "url": "/burger-builder/static/js/2.dc799eb5.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "42b413c73d847e57cb2c2776edfdea85",
+    "url": "/burger-builder/index.html"
+  }
+];
